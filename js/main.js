07648058d5c8a9af4703ef9f7d0bf28833831389
dcastroe <dilica84@hotmail.com>
2015-05-12 15:04:23 +0200

    ny js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ String.prototype.replaceAt = function (index, character) {
 	return this.substr(0, index) + character + this.substr(index + character.length);
 }
 
-
 $(document).ready(function() {
-	showPage();
+	showPage();	
 });
 
 function lolcoppter() {
+	console.log("lol");
 	var rofl = document.getElementById('rofl').innerHTML;
 		chMap = [
 			[0, 1, 2, 3, 4, 5, 6, 7, 8, 43, 78],
@@ -20,6 +20,7 @@ function lolcoppter() {
 		isBlankCounter = 1,
 		nonBlankTemplate = ['R', 'O', 'F', 'L', ':', 'R', 'O', 'F', 'L', 'L', 'L'];
 		
+	setInterval(doRofl, 80);
 
 	function doRofl() {
 		for(var i = 0; i < chMapLength; i++) {
@@ -33,13 +34,11 @@ function lolcoppter() {
 				}  
 				rofl = rofl.replaceAt(chMap[i][j], chReplace);
 			}
-
 			document.getElementById('rofl').innerHTML = rofl;
 		}  
-	}
-
-	setInterval(doRofl, 80);	
+	}		
 }
+
 
 function showPage() {
 	$('#menu li a').click(function() {
@@ -47,53 +46,44 @@ function showPage() {
 		$("#content").slideUp();
 
 		window.setTimeout(function() {
-			var request = new XMLHttpRequest();
-			request.open("GET", page, true);
-			request.send();
-			request.onreadystatechange = function() {
-				if(request.readyState == 4 && request.status == 200) {
-					document.getElementById("content").innerHTML = request.responseText;
-					$("#content").slideDown();
+			$.get(page, function(data) {
+				$("#content").html(data);
+				$("#content").slideDown();
+
+				if(page == 'conditions.html'){
+					lolcoppter();
+				} else if(page == 'contact.html') {
+					validate();
 				}
-			}
+			});
 		}, 500);
 		return false;
-		if(page == 'conditions.html'){
-			lolcoppter();
-		}
 	});
 }
 
-function sendForm() {
-	if(validatephone()) {
-		if(document.getElementById("name").value == "") {
+function validate() {
+	$('#send').click(function(e) {
+
+		var em = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+		var r = em.exec(document.getElementById("email").value);
+		var phonepat = /[0-9-()+]{9}/;
+		var result = phonepat.exec(document.getElementById("tfn").value);
+	
+		if($('input').val() == '') {
 			alert("Ditt meddelande har inte skickats.");
-		} else {
+		} else if(r && result) {
+			$("#email, #tfn").css('outline', '2px solid green');
 			alert("Tack! Ditt meddelande har skickats.");
 			window.location.href="index.html";
-		} 
-	} else {
-		alert("Ditt meddelande har inte skickats.");
-	}
-
-	function validatephone() {
-		return validateemail();
-		function validateemail() {
-			var em = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-			var r = em.exec(document.getElementById("email").value);
-			if(!r) {
-				$("#email").css('outline', '1px solid red');
-			} else if (r) {
-				$("#email").css('outline', '2px solid green');
-			} 
-			else {
-				var phonepat = /^([+]46)\s*(7[0236])\s*(\d{4})\s*(\d{3})$/;
-				var result = phonepat.exec(document.getElementById("tfn").value);
-				if(!result) $("#tfn").css('outline', '1px solid red');
-				if(result) $("#tfn").css('outline', '2px solid green');
-				if(!result) return false;
-				else return true;
-			}
-		}
-	}
+		} else if (!r ) {
+			$("#email").css('outline', '1px solid red');
+			alert("Ogiltig email angivit.");
+		} else if(!result) {
+			$("#tfn").css('outline', '1px solid red');
+			alert("Ogiltig telefonnummer angivit.");
+		}  
+		
+		e.preventDefault();
+	});
 }
+
